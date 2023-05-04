e07f713ed46516b068c5c84007beb7954bc7e66d
rockyrelay <81175151+rockyrelay@users.noreply.github.com>
2023-05-04 19:30:02 +0100

    fetch to random character

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-let baseURL = "https://swapi.dev/api/";
-let person = document.getElementById()
+let baseURL = "https://swapi.dev/api/people/";
+let person = document.getElementById("character")
+let charNumber = Math.floor((Math.random() * 83) + 1)
 
-fetch(baseURL)
+fetch(baseURL + charNumber)
   .then((response) => response.json())
   .then((json) => {
     console.log(json)
-
+    person.innerHTML = `<h2>${json.name}</h2>`
   });
