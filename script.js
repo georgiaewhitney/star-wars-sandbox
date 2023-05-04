c8cfcbe3c8e324648a58f018fe210ca6c437fa4e
rockyrelay <81175151+rockyrelay@users.noreply.github.com>
2023-05-04 19:50:21 +0100

    added styling and loading functionality

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const baseURL = "https://swapi.dev/api/people/";
-const person = document.getElementById("character")
-const button = document.getElementById("charButton")
+const person = document.getElementById("character");
+const button = document.getElementById("charButton");
+const loading = document.getElementById("loading");
 
 function generateCharacter() {
-  let charNumber = Math.floor((Math.random() * 83) + 1);
+  button.disabled = true;
+  loading.style.display = "block";
+  let charNumber = Math.floor(Math.random() * 83 + 1);
   fetch(baseURL + charNumber)
-  .then((response) => response.json())
-  .then((json) => {
-    console.log(json)
-    person.innerHTML = `
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      person.innerHTML = `
       <h2>${json.name}</h2>
       <h3>Born: ${json.birth_year}</h3>
-    `
-  });
+    `;
+      loading.style.display = "none";
+      button.disabled = false;
+    });
 }
-
-
